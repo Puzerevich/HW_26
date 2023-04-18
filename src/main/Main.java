65386b40ed08b1b1264154c53c4d33cdd145b71b
Puzerevich <puzerevich@gmail.com>
2023-04-18 23:19:23 +0300

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,16 @@
 package main;
 
-public class Main
-{
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Main{
+    public static void main(String[] args) {
+
+        ArrayList<Object> orders = new ArrayList<>();
+        Order ord1 = new Order(1, new Date(), 15.0, new ArrayList<>());
+        Order ord2 = new Order(2, new Date(), 22.0, new ArrayList<>());
+        orders.add(ord1);
+        orders.add(ord2);
+
+    }
 }
