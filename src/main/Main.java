b6faab471368f71048a263d33c6a6716b86073e6
Puzerevich <puzerevich@gmail.com>
2023-04-18 23:19:36 +0300

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,12 +5,10 @@ import java.util.Date;
 
 public class Main{
     public static void main(String[] args) {
-
         ArrayList<Object> orders = new ArrayList<>();
         Order ord1 = new Order(1, new Date(), 15.0, new ArrayList<>());
         Order ord2 = new Order(2, new Date(), 22.0, new ArrayList<>());
         orders.add(ord1);
         orders.add(ord2);
-
     }
 }
